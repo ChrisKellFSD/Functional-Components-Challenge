2e46aedcf368695775049bc14ae9f276c073158a
ChrisKellFSD <chriskellcsn@gmail.com>
2023-01-09 16:41:42 +0000

    Correct solution

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-const Sidebar = () =>
-<div><a>My Photos</a><a>My Illustrations</a><a>My Paintings</a></div>
+function Sidebar() {
+    return (
+        <div>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+        </div>
+    )
+}
 
 export default Sidebar
